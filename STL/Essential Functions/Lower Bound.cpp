1001a075f6c1fa7411f2d34438328b17db9cb3e4
Saubhagya Chopra <75625298+saubhagya0111@users.noreply.github.com>
2021-08-18 21:02:05 +0530

    Create Lower Bound.cpp

---
+++ STL/Essential Functions/Lower Bound.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    vector<int> A = {1, 5, 9, 10, 22};
+    int n = sizeof(A) / sizeof(A[0]);
+    sort(A.begin(), A.end());
+    vector<int>::iterator i1, i2, i3;
+    i1 = lower_bound(A.begin(), A.end(), 22);
+    //Used to return the index of the element searched in the data structure
+    //If multiple values are present then it returns the value of the index with the first occurrence
+    //In case the element is not found it gives out the index of the element just smaller than the key
+    //Passes the value to the iterator and the index can be printed by subtracting the value of the iterator from the begin function
+    cout << i1 - A.begin() << endl;
+    // cout << binary_search(A, A + n, 10) << endl;
+}

cbea89995d45fb350562375611fcdffaa792bb1f
Saubhagya Chopra <75625298+saubhagya0111@users.noreply.github.com>
2021-08-31 20:30:26 +0530

    Update Code.cpp

--- Number Theory/Euclid GCD Algorithm/Code.cpp
+++ Number Theory/Euclid GCD Algorithm/Code.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 int euclid_gcd(int a, int b)
 {
+//considering the scenario where the second number is larger than the first one
+    if (a < b)
+    {
+        return euclid_gcd(b, a);
+    }
     //Mathmeatical interpretation of the euclid's gcd algorithm
     //gcd(a,b)=gcd(b,a%b)
     //Defining base condition of the recursive call to find the gcd
+
     if (b == 0)
     {
         //hcf of the numbers when one of them is zero is the other number
